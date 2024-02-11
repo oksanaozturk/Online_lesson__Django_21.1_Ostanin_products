ab2b11611d100bf40b9cd85a272180858a446687
casper <oksanaozturk@gmail.com>
2024-02-12 00:21:04 +0300

    Add home_page and contacts

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, 'catalog/home_page.html')
+
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
