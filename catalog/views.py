5871e0560407a8ca9a85e6da995a5ca45e480fe5
casper <oksanaozturk@gmail.com>
2024-02-12 08:46:06 +0300

    Update views.py (add metHod POST)

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,11 @@ def home_page(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # а также передается информация, которую заполнил пользователь
+        print(name, phone, message)
     return render(request, 'catalog/contacts.html')
