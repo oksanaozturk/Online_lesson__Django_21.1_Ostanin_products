c1bd13785aea3fff63847337d5103d9857da1d1f
casper <oksanaozturk@gmail.com>
2024-02-27 12:07:21 +0300

    Добавлены пути для products_list и product_detail, удален путь для home_page (вместо него стал products_list)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from catalog.views import home_page, contacts
+from catalog.views import contacts, products_list, product_detail
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
-    path('contacts/', contacts, name='contacts')
+    path('', products_list, name='products_list'),
+    # path('', home_page, name='home_page'),
+    path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>/', product_detail, name='product_detail')
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Другой вариант прописывания media путей
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
