6588872b6c79aecbfbdbccc91d76df3008314f6f
casper <oksanaozturk@gmail.com>
2024-02-27 12:04:09 +0300

    Add products_list and product_detail, delete home_page

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
-def home_page(request):
-    return render(request, 'catalog/home_page.html')
+def products_list(request):
+    """Вывод страницу со всеми продуктами"""
+    products = Product.objects.all()
+    context = {
+        'object_list': products
+    }
+    return render(request, 'catalog/products_list.html', context)
+
+
+def product_detail(request, pk):
+    """Вывод страницу с одним продуктом по pk"""
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': product
+    }
+    return render(request, 'catalog/product_detail.html', context)
+
+# Заменили выведелие главной страницы на products_list
+# def home_page(request):
+#     products = Product.objects.all()
+#     context = {
+#         'object_list': products
+#     }
+#     return render(request, 'catalog/home_page.html', context)
+#     # return render(request, 'catalog/home_page.html')
 
 
 def contacts(request):
