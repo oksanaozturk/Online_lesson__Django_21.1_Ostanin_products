a4a9b070fb734ba8bd8b700849a4e5e032fca23d
casper <oksanaozturk@gmail.com>
2024-02-27 12:05:17 +0300

    Add <app_name> and namespace

--- config/urls.py
+++ config/urls.py
@@ -16,15 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Другой вариант прописывания media путей
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    path('', include(('catalog.urls', 'catalog'), 'catalog'))
+]
