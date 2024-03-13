321538e6163b44a5b81a62e7c7e9969e882ffe23
casper <oksanaozturk@gmail.com>
2024-03-13 16:12:41 +0300

    Обновление, удаление ненужных ссылок

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
