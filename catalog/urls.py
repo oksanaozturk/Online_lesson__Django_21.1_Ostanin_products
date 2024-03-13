5c0936466ed74eacb0e8e4d63d990e1635869733
casper <oksanaozturk@gmail.com>
2024-03-13 16:00:47 +0300

    Добавлены пути для созданных новых классов ProductListView (для вывода всех товаров), ProductDetailView (для постраничного выведения товаров), ProductCreateView (для создания нового продукта), ProductUpdateView (для обновления данных товара), ProductDeleteView (для удаления товара)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from catalog.views import contacts, products_list, product_detail
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', products_list, name='products_list'),
+    path('', ProductListView.as_view(), name='products_list'),
     # path('', home_page, name='home_page'),
     path('contacts/', contacts, name='contacts'),
-    path('products/<int:pk>/', product_detail, name='product_detail')
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Другой вариант прописывания media путей
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
